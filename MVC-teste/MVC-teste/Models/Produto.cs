4a8dfb2a20cd0f7fb38c63b4e3344eaf51bafa3d
Allysom Oliveira <allysomoliveira7@gmail.com>
2020-06-16 15:14:40 -0300

    Realizado orgarnização e aplicação de novas categorias ao projeto.

--- MVC-teste/MVC-teste/Models/Produto.cs
+++ MVC-teste/MVC-teste/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace MVC_teste.Models
     public class Produto
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage ="O campo descrição é obrigatório !")]
         public string Descricao { get; set; }
+        [Range(1,100, ErrorMessage = "Valor fora da faixa !")]
         public int Quantidade { get; set; }
         public int CategoriaId { get; set; }
         public List<Produto> produtos { get; set; }
